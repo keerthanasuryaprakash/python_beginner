1216dbc993898cc8dcdbe50df4b64d83f2cdc705
keerthanasuryaprakash <32951395+keerthanasuryaprakash@users.noreply.github.com>
2017-10-21 15:52:15 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,22 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16cse017
+#
+# Created:     21/10/2017
+# Copyright:   (c) 16cse017 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+counter=100
+miles  =1000.0
+name   ="John"
+print counter
+print miles
+print name
